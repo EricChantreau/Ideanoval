72fbed292fc9263417310b20e05d546c030c381e
CharlesCarbonne <charlescarbonne@hotmail.fr>
2017-10-09 14:47:45 +0200

    Création AdminServices.java

---
+++ src/fr/humanbooster/services/AdminServices.java
@@ -0,0 +1,36 @@
+package fr.humanbooster.services;
+
+import fr.humanbooster.ideas.Category;
+import fr.humanbooster.ideas.Comment;
+import fr.humanbooster.ideas.Idea;
+import fr.humanbooster.users.User;
+
+public interface AdminServices {
+
+//	Liste des actions réalisables par l'admin:
+//	
+//	Valider inscription utilisateur
+//	Consulter liste d'alertes
+//	désactiver une idée
+//	désactiver un commentaire
+//	désactiver un utilisateur
+//	supprimer un utilisateur
+//	Ajouter catégorie
+//	Supprimer catégorie
+//	Modifier catégorie
+	
+	
+	public void signUpValidation(User user);
+//	public Alert getAlerts(List<Alert> alerts);
+	public void disableIdea(Idea idea);
+	public void disableComment(Comment comment);
+	public void diableUser(User user);
+	public void deleteUser(User user);
+	public Category addCategory();
+	public void deleteCategory(Category category);
+	public void customizeCategory(Category category);
+	
+	
+	
+	
+}
