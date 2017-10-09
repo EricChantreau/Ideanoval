5c1688fd17800aa28ed5f1f64a1fa6941c55e5aa
EricChantreau <e.chantreau@gmail.com>
2017-10-09 15:36:39 +0200

    relocated LoginServices

--- src/fr/humanbooster/services/UserServices.java
+++ src/fr/humanbooster/services/UserServices.java
@@ -6,10 +6,6 @@ import fr.humanbooster.ideas.Survey;
 import fr.humanbooster.users.User;
 
 public interface UserServices {
-	public boolean register(User user);
-
-	public User connect(String login, String password);
-
 	public void voteForIdea(User user, Idea idea, Eval eval);
 
 	public void answerToSurvey(User user, Survey survey, int optionIndex);
--- src/fr/humanbooster/services/UserServicesImpl.java
+++ src/fr/humanbooster/services/UserServicesImpl.java
@@ -1,43 +1,13 @@
 package fr.humanbooster.services;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import fr.humanbooster.ideas.Answer;
 import fr.humanbooster.ideas.Eval;
 import fr.humanbooster.ideas.Idea;
 import fr.humanbooster.ideas.Survey;
 import fr.humanbooster.ideas.Vote;
-import fr.humanbooster.users.Member;
 import fr.humanbooster.users.User;
 
 public class UserServicesImpl implements UserServices {
-
-	private List<User> users;
-
-	public UserServicesImpl() {
-		users = new ArrayList<>();
-		users.add(new Member("echantreau", "password0", "eric c", true));
-	}
-
-	@Override
-	public boolean register(User user) {
-		if (users.contains(user))
-			return false;
-
-		users.add(user);
-		return true;
-	}
-
-	@Override
-	public User connect(String login, String password) {
-		for (User user : users) {
-			if (user.getEmail().equals(login) && user.getPassword().equals(password))
-				return user;
-		}
-
-		return null;
-	}
 
 	@Override
 	public void voteForIdea(User user, Idea idea, Eval eval) {
