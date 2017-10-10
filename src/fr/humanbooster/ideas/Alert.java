6fc1fa350e3848a5481d7b92f1855d2296f1e6a6
CharlesCarbonne <charlescarbonne@hotmail.fr>
2017-10-10 10:18:19 +0100

    commit

--- src/fr/humanbooster/ideas/Alert.java
+++ src/fr/humanbooster/ideas/Alert.java
@@ -11,6 +11,29 @@ public class Alert {
 	public Member member;
 	public Date date;
 	public Post post;
+	public Member getMember() {
+		return member;
+	}
+	public void setMember(Member member) {
+		this.member = member;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Post getPost() {
+		return post;
+	}
+	public void setPost(Post post) {
+		this.post = post;
+	}
+	
+	@Override
+	public String toString() {
+		return "Alert [member=" + member + ", date=" + date + ", post=" + post + "]";
+	}
 	
 
 }
